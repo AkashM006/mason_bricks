154d2dc75ac02014ef5b096b2ced58c3bd7a35c7
Akash M <akashm006@outlook.com>
2025-01-26 15:34:34 +0530

    Startup brick initial setup complete

---
+++ startup_hooks/__brick__/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// generated but startup with hooks
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text("Home"),
+          ),
+        ),
+      );
+}
