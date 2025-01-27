a1493b81819b998572186982eb78081fd8eb27ea
Akash M <akashm006@outlook.com>
2025-01-27 20:58:34 +0530

    Now running process in shell

--- core_init/hooks/post_gen.dart
+++ core_init/hooks/post_gen.dart
@@ -5,7 +5,7 @@ import 'package:mason/mason.dart';
 void run(HookContext context) {
   final progress = context.logger.progress('Installing required packages...');
 
-  Process.run('flutter', ['pub', 'add', 'intl', 'uuid']);
+  Process.run('flutter', ['pub', 'add', 'intl', 'uuid'], runInShell: true);
 
   progress.complete();
 }
