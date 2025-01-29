165852018f0c6930c5f41fbdcc6042452b66ad89
Akash M <akashm006@outlook.com>
2025-01-29 16:20:13 +0530

    Moved making core_init to post_gen in shared_init

--- shared_init/hooks/pre_gen.dart
+++ shared_init/hooks/pre_gen.dart
@@ -1,25 +1,3 @@
-import 'dart:io';
-
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {
-  final projectName = context.vars['project_name'] as String;
-  final currentDirectory = Directory.current;
-
-  // need to initialize core_init for this package
-  final coreProgress = context.logger.progress("Making core_init....");
-
-  final generator = await MasonGenerator.fromBrick(
-    Brick.git(
-      GitPath('https://github.com/AkashM006/mason_bricks', path: 'core_init'),
-    ),
-  );
-
-  await generator.generate(
-    DirectoryGeneratorTarget(currentDirectory),
-    vars: {'project_name': projectName},
-    fileConflictResolution: FileConflictResolution.overwrite,
-  );
-
-  coreProgress.complete();
-}
+void run(HookContext context) async {}
