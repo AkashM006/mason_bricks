68ec1e055b90e2cb27cc61a87e52d06f6f0fbaff
Akash M <akashm006@outlook.com>
2025-01-29 17:16:47 +0530

    Calling post_gen hook of core_init in shared_init

--- shared_init/hooks/post_gen.dart
+++ shared_init/hooks/post_gen.dart
@@ -3,28 +3,6 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
-  final projectName = context.vars['project_name'] as String;
-  final currentDirectory = Directory.current;
-
-  // need to initialize core_init for this package
-  final coreProgress =
-      context.logger.progress("Making core_init with hooks....");
-
-  final generator = await MasonGenerator.fromBrick(
-    Brick.git(
-      GitPath('https://github.com/AkashM006/mason_bricks', path: 'core_init'),
-    ),
-  );
-
-  await generator.generate(
-    DirectoryGeneratorTarget(currentDirectory),
-    vars: {'project_name': projectName},
-    fileConflictResolution: FileConflictResolution.overwrite,
-    logger: context.logger,
-  );
-
-  coreProgress.complete();
-
   final formattingProgress =
       context.logger.progress("Formatting generated code...");
 
--- shared_init/hooks/pre_gen.dart
+++ shared_init/hooks/pre_gen.dart
@@ -1,3 +1,28 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {}
+void run(HookContext context) async {
+  final projectName = context.vars['project_name'] as String;
+  final currentDirectory = Directory.current;
+
+  // Initializing core_init brick since this brick needs it
+  final coreProgress =
+      context.logger.progress("Making core_init with hooks....");
+
+  final generator = await MasonGenerator.fromBrick(
+    Brick.git(
+      GitPath('https://github.com/AkashM006/mason_bricks', path: 'core_init'),
+    ),
+  );
+
+  await generator.generate(
+    DirectoryGeneratorTarget(currentDirectory),
+    vars: {'project_name': projectName},
+    fileConflictResolution: FileConflictResolution.overwrite,
+  );
+
+  await generator.hooks.postGen();
+
+  coreProgress.complete();
+}
