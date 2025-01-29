508141ef63698fe3f020b2fae40486734ea31c40
Akash M <akashm006@outlook.com>
2025-01-29 16:04:07 +0530

    Minor change to core_init

--- core_init/hooks/post_gen.dart
+++ core_init/hooks/post_gen.dart
@@ -2,10 +2,14 @@ import 'dart:io';
 
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
+void run(HookContext context) async {
   final progress = context.logger.progress('Installing required packages...');
 
-  Process.run('flutter', ['pub', 'add', 'intl', 'uuid'], runInShell: true);
+  await Process.run(
+    'flutter',
+    ['pub', 'add', 'intl', 'uuid'],
+    runInShell: true,
+  );
 
   progress.complete();
 }
