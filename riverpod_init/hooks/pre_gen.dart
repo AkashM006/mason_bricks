f980fc48bfa5b8a016f63a68d697da52d4131e86
Akash M <akashm006@outlook.com>
2025-01-31 20:49:10 +0530

    New implementation of riverpod brick

--- riverpod_init/hooks/pre_gen.dart
+++ riverpod_init/hooks/pre_gen.dart
@@ -1,27 +1,3 @@
-import 'dart:io';
-
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {
-  final projectName = context.vars['project_name'];
-
-  final coreProgress = context.logger.progress("Making core_init brick....");
-  final currentDirectory = Directory.current;
-
-  final generator = await MasonGenerator.fromBrick(
-    Brick.git(
-      GitPath("https://github.com/AkashM006/mason_bricks", path: 'core_init'),
-    ),
-  );
-
-  final vars = {'project_name': projectName};
-
-  await generator.generate(
-    DirectoryGeneratorTarget(currentDirectory),
-    vars: vars,
-  );
-
-  await generator.hooks.postGen(vars: vars);
-
-  coreProgress.complete();
-}
+void run(HookContext context) async {}
