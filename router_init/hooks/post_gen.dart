4c8a83c89cf41f535d7e0cb70ab3a69cb1c3383a
Akash M <akashm006@outlook.com>
2025-02-01 19:18:33 +0530

    Minor changes to post_gen

--- router_init/hooks/post_gen.dart
+++ router_init/hooks/post_gen.dart
@@ -5,7 +5,7 @@ import 'package:mason/mason.dart';
 void run(HookContext context) async {
   final routerProgress = context.logger.progress("Installing go_router");
 
-  await Process.run('flutter', ['pub', 'add', 'go_router']);
+  await Process.run('flutter', ['pub', 'add', 'go_router'], runInShell: true);
 
   routerProgress.complete();
 }
