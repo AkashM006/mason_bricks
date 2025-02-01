d17535156718db3cce251daf0e3c384e4efc759c
Akash M <akashm006@outlook.com>
2025-02-01 18:35:53 +0530

    Completed core_init with the new implementation

--- core_init/__brick__/lib/src/core/constants/settings.dart
+++ core_init/__brick__/lib/src/core/constants/settings.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 
 const kAppName = '{{ project_name.pascalCase() }}';
 const kSeedColor = Colors.blue;
+{{#uses_db}}
+const kHideDebugDb = false;
+{{/uses_db}}
