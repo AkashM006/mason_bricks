e1257f9014a3aafe81bba96b0fbe3ff0f44d233a
Akash M <akashm006@outlook.com>
2025-02-02 12:21:40 +0530

    Changed formatting process sequence

--- main_init/hooks/post_gen.dart
+++ main_init/hooks/post_gen.dart
@@ -7,6 +7,14 @@ void run(HookContext context) async {
   final usesRiverpod = context.vars['uses_riverpod'] as bool;
   final usesDb = context.vars['uses_db'] as bool;
 
+  if (!usesRiverpod && !usesDb) return;
+
+  final buildingProgress = context.logger.progress("Building code...");
+
+  await Process.run('dart', ['run', 'build_runner', 'build']);
+
+  buildingProgress.complete();
+
   final formattingProgress =
       context.logger.progress("Formatting generated code...");
 
@@ -17,12 +25,4 @@ void run(HookContext context) async {
   );
 
   formattingProgress.complete();
-
-  if (!usesRiverpod && !usesDb) return;
-
-  final buildingProgress = context.logger.progress("Building code...");
-
-  await Process.run('dart', ['run', 'build_runner', 'build']);
-
-  buildingProgress.complete();
 }
