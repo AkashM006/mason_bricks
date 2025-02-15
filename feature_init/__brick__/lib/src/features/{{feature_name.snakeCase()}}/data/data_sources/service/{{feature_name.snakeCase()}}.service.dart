7c66e434e86c55cdaa55fce84e2473ddceb11337
Akash M <akashm006@outlook.com>
2025-02-15 08:49:59 +0530

    Added repository methods

--- feature_init/__brick__/lib/src/features/{{feature_name.snakeCase()}}/data/data_sources/service/{{feature_name.snakeCase()}}.service.dart
+++ feature_init/__brick__/lib/src/features/{{feature_name.snakeCase()}}/data/data_sources/service/{{feature_name.snakeCase()}}.service.dart
@@ -8,10 +8,15 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part '{{feature_name.snakeCase()}}.service.g.dart';
 
 class {{feature_name.pascalCase()}}Service {
+  {{#uses_db}}
   final AppDatabase _appDatabase;
+  {{/uses_db}}
 
+  {{#uses_db}}
   const {{feature_name.pascalCase()}}Service(this._appDatabase);
+  {{/uses_db}}
 
+  {{#uses_db}}
   Stream<List<{{feature_name.pascalCase()}}Entity>> watch{{feature_name.pascalCase()}}() =>
       _appDatabase.{{feature_name.camelCase()}}Dao.watch{{feature_name.pascalCase()}}().map(
             (models) => models
@@ -20,11 +25,14 @@ class {{feature_name.pascalCase()}}Service {
                 )
                 .toList(),
           );
+  {{/uses_db}}
 }
 
 @riverpod
 {{feature_name.pascalCase()}}Service {{feature_name.camelCase()}}Service (Ref ref) {
+  {{#uses_db}}
   final db = ref.watch(dbProvider);
 
   return {{feature_name.pascalCase()}}Service(db);
+  {{/uses_db}}
 }
---
+++ feature_init/__brick__/lib/src/features/{{feature_name.snakeCase()}}/data/repository/{{feature_name.snakeCase()}}_impl.repository.dart
@@ -0,0 +1,24 @@
+import 'package:{{project_name.snakeCase()}}/src/features/shared/data/repository/base.repository.dart';
+import 'package:{{project_name.snakeCase()}}/src/features/{{feature_name.snakeCase()}}/data/data_sources/service/{{feature_name.snakeCase()}}.service.dart';
+import 'package:{{project_name.snakeCase()}}/src/features/{{feature_name.snakeCase()}}/domain/entity/{{feature_name.snakeCase()}}.entity.dart';
+import 'package:{{project_name.snakeCase()}}/src/features/{{feature_name.snakeCase()}}/domain/repository/{{feature_name.snakeCase()}}.repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part '{{feature_name.snakeCase()}}_impl.repository.g.dart';
+
+class {{feature_name.pascalCase()}}Impl extends BaseRepository implement {{feature_name.pascalCase()}}Repository {
+  final {{feature_name.pascalCase()}}Service _{{feature_name.camelCase()}}Service;
+
+  const {{feature_name.pascalCase()}}Impl(this._{{feature_name.camelCase()}}Service);
+
+  @override
+  Stream<List<{{feature_name.pascalCase()}}Entity>> watch{{feature_name.pascalCase()}}() => _{{feature_name.camelCase()}}Service.watch{{feature_name.pascalCase()}}();
+}
+
+@riverpod
+{{feature_name.pascalCase()}}Impl {{feature_name.camelCase()}}(Ref ref) {
+  final {{feature_name.camelCase()}}Service = ref.watch({{feature_name.camelCase()}}ServiceProvider);
+
+  return {{feature_name.pascalCase()}}Impl({{feature_name.camelCase()}}Service);
+}
